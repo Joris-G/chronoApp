60a971f20f152d1fde8b583aa694b3275b7e3a03
Joris G <joris-grangier@orange.fr>
2024-11-04 13:27:47 +0100

    home page layout update with app-content component

--- src/app/_pages/home/home.page.ts
+++ src/app/_pages/home/home.page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { LayoutModule } from '../../_core/_components/layouts/layout.module';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, LayoutModule],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss'
 })
